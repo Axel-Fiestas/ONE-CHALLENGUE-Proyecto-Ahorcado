b112b3b5f88dc4d3e387b555fc6e844ebbb9b012
Axekran <josue123452002@gmail.com>
2022-09-09 19:44:12 -0500

    Empezando a dar estilos

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -23,16 +23,18 @@ const wrongLetters=[]
 var hangmanFlag=0;
 
 
-
 const dScreen=document.querySelector(".hangman-draw");
 const dWrongLetters=document.querySelector('.error-words');
 const pincel= dScreen.getContext("2d");
 
+//Button click
+const startButton= document.getElementById("start");
+startButton.addEventListener("click",function (){
+    document.getElementById("initial-part").style.display = "none";
+    document.getElementById("game-part").style.display="flex";
+});
+
 showDashes(secretWord);
-
-
-
-
 
 document.addEventListener("keypress", function(event){
 
